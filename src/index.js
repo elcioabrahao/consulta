6d12e381caec3eca351fcb12e2c4ca5d844ec967
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-04-15 13:26:30 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -12,14 +12,23 @@ const funcoes = {
         observacoes.push(observacao);
         baseConsulta[observacao.lembreteId]["observacoes"] =
             observacoes;
-    }
+    },
+    ObservacaoAtualizada: (observacao) => {
+        const observacoes =
+            baseConsulta[observacao.lembreteId]["observacoes"];
+        const indice = observacoes.findIndex((o) => o.id ===
+            observacao.id);
+        observacoes[indice] = observacao;
+    },
 };
 
 app.get("/lembretes", (req, res) => {
     res.status(200).send(baseConsulta);
 });
 app.post("/eventos", (req, res) => {
-    funcoes[req.body.tipo](req.body.dados);
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
     res.status(200).send(baseConsulta);
 });
 app.listen(6000, () => console.log("Consultas. Porta 6000"));
