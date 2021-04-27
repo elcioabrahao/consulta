41657d7b57150da140d2b4229743e2e7ed25cf7d
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-04-27 19:41:20 -0300

    correções

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const baseConsulta = {};
+const axios = require("axios");
 const funcoes = {
     LembreteCriado: (lembrete) => {
         baseConsulta[lembrete.contador] = lembrete;
@@ -29,6 +30,20 @@ app.post("/eventos", (req, res) => {
     try {
         funcoes[req.body.tipo](req.body.dados);
     } catch (err) {}
+
     res.status(200).send(baseConsulta);
 });
-app.listen(6000, () => console.log("Consultas. Porta 6000"));
+app.listen(6000, async () => {
+    console.log("Consultas. Porta 6000");
+    const resp = await
+    axios.get("http://localhost:10000/eventos")
+        .catch((err) => {
+            console.log("err", err);
+        });
+    //axios entrega os dados na propriedade data
+    resp.data.forEach((valor, indice, colecao) => {
+        try {
+            funcoes[valor.tipo](valor.dados);
+        } catch (err) {}
+    });
+});
